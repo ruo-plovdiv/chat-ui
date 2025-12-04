03e8e6eff658e174713db2a0f49b304fd70c14ba
unknown <ruogithub@gmail.com>
2025-12-04 16:46:02 +0200

    init

---
+++ src/Header.js
@@ -0,0 +1,8 @@
+const Header = () => {
+    return <div>
+        <img src="./images/logo.webp" alt="лого"/>
+    </div>
+}
+
+
+export default Header;
---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+const Header = () => {
+    return <div className="container mt-5">
+        <div className="d-flex align-items-center">
+            <img src="./images/logo.webp" alt="лого"/>
+            <div className="ms-3 fs-2">
+               <span className="d-block fs-3 line-height-">
+                 Министерство на образованието и науката
+               </span>
+               <span className="fs-5">Регионално управление на образованието Пловдив</span>
+            </div>
+        </div>
+    </div>
+}   
+
+export default Header;
